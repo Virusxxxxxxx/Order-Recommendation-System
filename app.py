f9802d39d4c859b6557c381fd091cca7f7cd0f7b
nix18 <kyj20167@gmail.com>
2022-04-16 19:15:21 +0800

    fix 修复docs页面cdn崩溃问题

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, applications
+from fastapi.openapi.docs import get_swagger_ui_html
 
 from dao.userDao import userDao
 from dataStructure.requestDomain import User, Meal, Comment, Order
 from utils.validateUtil import tokenGen, validateToken, tokenParse
+
+
+# 由于jsdelivr老崩溃，换CDN解决docs崩溃
+def swagger_monkey_patch(*args, **kwargs):
+    """
+    Wrap the function which is generating the HTML for the /docs endpoint and
+    overwrite the default values for the swagger js and css.
+    """
+    return get_swagger_ui_html(
+        *args, **kwargs,
+        swagger_js_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/4.10.3/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.bootcdn.net/ajax/libs/swagger-ui/4.10.3/swagger-ui.css")
+
+
+# Actual monkey patch
+applications.get_swagger_ui_html = swagger_monkey_patch
 
 app = FastAPI()
 
